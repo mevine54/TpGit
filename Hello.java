2fa021b0f7bec636781d9f0e1098bf17d34aa7d8
mevine54 <mevinecascadeur@gmail.com>
2024-09-30 11:31:30 +0200

    Collaboration action 2

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void ecrire(String[] args) {
+       ecrire("Hello World");
+        
+    }
+
+    public void ecrire("Hello World");
+    // Essai collaboration 2
+}
